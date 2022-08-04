318dde84d72713fc55661f8777a5a012cc4cf610
Ravikant Mishra <94540021+navodayan14@users.noreply.github.com>
2022-08-04 11:46:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MintNfts from './pages/MintNfts';
+import Register_Company from './pages/Register_Company';
+import Multiple_qr from './pages/Printqrcodes';
+import Productdetail from './pages/Ownership';
+function App() {
+  return (
+    <div className="App">
+      <Productdetail/>
+    </div>
+  );
+}
+
+export default App;
